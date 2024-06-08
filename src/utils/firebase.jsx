47d806754fde2a17dcf15b1b689bd5a70fcab35c
MLara29 <143139611+MLara29@users.noreply.github.com>
2024-06-08 08:36:45 -0300

    Update firebase.jsx

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -2,14 +2,23 @@
 import { initializeApp } from 'firebase/app'
 import { collection, doc, getDocs, getFirestore, query, writeBatch } from 'firebase/firestore'
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID ,
   appId: import.meta.env.VITE_APP_ID,
-}
+} */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATTUmYeeqqWXifV1iJ5FOFo1Fa_lW6Uvo",
+  authDomain: "coder-eletronico-ecommerce.firebaseapp.com",
+  projectId: "coder-eletronico-ecommerce",
+  storageBucket: "coder-eletronico-ecommerce.appspot.com",
+  messagingSenderId: "155128635978",
+  appId: "1:155128635978:web:f4ce7ee659dc3b610e3cb3"
+};
 
 const app = initializeApp(firebaseConfig)
 
